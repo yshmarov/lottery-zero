37dddc92e6f6b1b9462863c29faeb2b2b5304eda
Yaro Shm <yshmarov@gmail.com>
2025-02-01 18:11:47 +0100

    tests pass

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,8 @@
-require 'test_helper'
+require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get landing_page" do
-    get static_pages_landing_page_url
+    get root_path
     assert_response :success
   end
-
 end
