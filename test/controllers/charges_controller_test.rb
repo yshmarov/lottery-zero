5191d63406b4fb31bc8369045562c594236e0354
Yaro Shm <yshmarov@gmail.com>
2025-02-01 20:10:09 +0100

    test charges

---
+++ test/controllers/charges_controller_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+
+class ChargesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
+  test "should get new" do
+    get new_charge_path
+    assert_response :success
+    assert_select "form[action=?]", charges_path
+  end
+
+  test "should create charge with valid params" do
+    assert_difference("Charge.count") do
+      post charges_path, params: { charge: { amount: 100 } }
+    end
+    assert_response :redirect
+  end
+
+  test "should not create charge with invalid params" do
+    assert_no_difference("Charge.count") do
+      post charges_path, params: { charge: { amount: nil } }
+    end
+
+    assert_response :unprocessable_entity
+  end
+end
