4e52f3d0e4ce448a34fe9aa9c97f3677a5bc3223
Yaro Shm <yshmarov@gmail.com>
2025-02-01 21:49:18 +0100

    better naming

--- test/controllers/basic_controller_test.rb
+++ test/controllers/basic_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+class BasicControllerTest < ActionDispatch::IntegrationTest
   test "should get landing_page" do
     get root_path
     assert_redirected_to new_user_session_path
